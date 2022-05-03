77afd3dfbd8089cb0987041a44f051d3f04cdf30
Johnny Irvin <irvinjohnathan@gmail.com>
2022-05-03 15:25:05 -0400

    Use updated syntaxes

--- world_attributer/__main__.py
+++ world_attributer/__main__.py
@@ -18,64 +18,57 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-from korth_spirit import EventEnum, Instance, aw_wait
-from korth_spirit.coords import Coordinates
+from korth_spirit import ConfigurableInstance, EventEnum
+from korth_spirit.configuration import JsonConfiguration
+from korth_spirit.sdk.enums import EventEnum
 
 from utilities import (every_x, get_all_attributes, get_attribute,
                        if_begins_with, set_attribute)
 
-with Instance(name=input("Bot Name: ")) as bot:
-    try:
-        bot.login(
-            citizen_number=(int(input("Citizen Number: "))),
-            password=input("Password: ")
-        ).subscribe(
-            EventEnum.AW_EVENT_CHAT,
-            lambda event: if_begins_with(
-                event=event,
-                beginning="/get attribute",
-                func=lambda attribute: bot.whisper(
+with ConfigurableInstance(
+    JsonConfiguration("configuration.json")
+) as bot:
+    bot.bus.subscribe(
+        EventEnum.AW_EVENT_CHAT,
+        lambda event: if_begins_with(
+            event=event,
+            beginning="/get attribute",
+            func=lambda attribute: bot.whisper(
+                session=event.chat_session,
+                message=get_attribute(
+                    instance=bot,
+                    attribute=attribute
+                )
+            )
+        )
+    ).subscribe(
+        EventEnum.AW_EVENT_CHAT,
+        lambda event: if_begins_with(
+            event=event,
+            beginning="/set attribute",
+            func=lambda command_string: bot.whisper(
+                session=event.chat_session,
+                message=set_attribute(
+                    instance=bot,
+                    attribute=command_string.split(" ")[0],
+                    value=" ".join(command_string.split(" ")[1:])
+                )
+            )
+        )
+    ).subscribe(
+        EventEnum.AW_EVENT_CHAT,
+        lambda event: if_begins_with(
+            event=event,
+            beginning="/get all attributes",
+            func=lambda _: every_x(
+                iterable=get_all_attributes(instance=bot),
+                x=5,
+                func=lambda attributes: bot.whisper(
                     session=event.chat_session,
-                    message=get_attribute(
-                        instance=bot,
-                        attribute=attribute
-                    )
+                    message="\n".join(f"{attr.name} = {attr.value}" for attr in attributes)
                 )
             )
-        ).subscribe(
-            EventEnum.AW_EVENT_CHAT,
-            lambda event: if_begins_with(
-                event=event,
-                beginning="/set attribute",
-                func=lambda command_string: bot.whisper(
-                    session=event.chat_session,
-                    message=set_attribute(
-                        instance=bot,
-                        attribute=command_string.split(" ")[0],
-                        value=" ".join(command_string.split(" ")[1:])
-                    )
-                )
-            )
-        ).subscribe(
-            EventEnum.AW_EVENT_CHAT,
-            lambda event: if_begins_with(
-                event=event,
-                beginning="/get all attributes",
-                func=lambda _: every_x(
-                    iterable=get_all_attributes(instance=bot),
-                    x=5,
-                    func=lambda attributes: bot.whisper(
-                        session=event.chat_session,
-                        message="\n".join(f"{attr.name} = {attr.value}" for attr in attributes)
-                    )
-                )
-            )
-        ).enter_world(
-            input("World: ")
-        ).move_to(Coordinates(0, 0, 0))
-    except Exception as e:
-        print("An error occurred:", e)
-        exit()
+        )
+    )
 
-    while True:
-        aw_wait(1)
+    bot.main_loop()
